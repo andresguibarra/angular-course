1d717aa33e4488e32ad54e63ff78406c17ecdc79
Andrés Guibarra <andresguibarra@gmail.com>
2022-10-03 16:56:18 -0300

    Modifying header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { FormsModule } from '@angular/forms'
 
-import { AppComponent } from './app.component';
-import { SuccessAlertComponent } from './task-one/success-alert/success-alert.component';
-import { WarningAlertComponent } from './task-one/warning-alert/warning-alert.component';
-import { TwoWayDatabindingComponent } from './task-two/two-way-databinding/two-way-databinding.component';
-import { TaskThreeComponent } from './task-three/task-three.component';
+import { AppComponent } from './app.component'
+import { SuccessAlertComponent } from './task-one/success-alert/success-alert.component'
+import { WarningAlertComponent } from './task-one/warning-alert/warning-alert.component'
+import { TwoWayDatabindingComponent } from './task-two/two-way-databinding/two-way-databinding.component'
+import { TaskThreeComponent } from './task-three/task-three.component'
+import { HeaderComponent } from './header/header.component'
+import { RecipesComponent } from './recipes/recipes.component'
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component'
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component'
+import { ShoppingListComponent } from './shopping-list/shopping-list.component'
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component'
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component'
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, SuccessAlertComponent, WarningAlertComponent, TwoWayDatabindingComponent, TaskThreeComponent],
-  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent,
+    TwoWayDatabindingComponent,
+    TaskThreeComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  isMenuOpen: boolean;
+  isHamburgerMenuOpen: boolean;
   constructor() { }
 
   ngOnInit() {

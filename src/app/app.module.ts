33f3f83560359774c6a12434c13308883d5a93bb
andresguibarra <andresguibarra@gmail.com>
2022-10-03 09:16:08 -0300

    Task one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { SuccessAlertComponent } from './task-one/success-alert/success-alert.component';
+import { WarningAlertComponent } from './task-one/warning-alert/warning-alert.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, SuccessAlertComponent, WarningAlertComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

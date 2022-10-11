a006381c8c2ef48a00ee316a41c5dff4c028b3ce
andresguibarra <andresguibarra@gmail.com>
2022-10-11 17:12:17 -0300

    Adding the shopping list Form

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -10,12 +10,12 @@ import { ShoppingListService } from './shopping-list.service';
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
-  private igChangedSub: Subscription;
+  private subscription: Subscription;
   constructor(private slService: ShoppingListService) {}
 
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
-    this.igChangedSub = this.slService.ingredientsChanged
+    this.subscription = this.slService.ingredientsChanged
       .subscribe(
         (ingredients: Ingredient[]) => {
           this.ingredients = ingredients;
@@ -23,7 +23,11 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
       )
   }
 
+  onEditItem(index: number) {
+    this.slService.startedEditing.next(index);
+  }
+
   ngOnDestroy(): void {
-    this.igChangedSub.unsubscribe();
+    this.subscription.unsubscribe();
   }
 }
